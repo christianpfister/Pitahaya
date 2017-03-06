e9315cbedf4ad615e227335593974b74ac5a5a64
Christian Pfister <christianpfister@bluewin.ch>
2017-03-06 14:32:21 +0100

    Changes @ hello.js & index

--- src/main/resources/static/hello.js
+++ src/main/resources/static/hello.js
@@ -1,7 +1,28 @@
-angular.module('demo', [])
-.controller('hello', function($scope, $http) {
+var app = angular.module('myproj', []);
+
+
+
+app.controller('showproj', function($scope, $http) {
     $http.get('http://localhost:4567/hello').
         then(function(response) {
             $scope.greeting = response.data;
         });
+});
+
+
+app.controller('showctrl', function($scope) {
+    $scope.showMe = false;
+    $scope.myFunc = function() {
+        $scope.showMe = !$scope.showMe;
+    }
+});
+
+
+app.controller('refresh_control',function($scope,$interval){
+var c=0;
+$scope.message="This DIV is refreshed "+c+" time.";
+$interval(function(){
+$scope.message="This DIV is refreshed "+c+" time.";
+c++;
+},1000);
 });
