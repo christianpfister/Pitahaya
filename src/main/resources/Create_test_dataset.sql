ee579428556e387821f030867c85612cc48b0c1f
Patrik Näf <patriknaef@bluemail.ch>
2017-03-18 12:43:28 +0100

    Standardwerte für Rollen und Projektstatus

--- src/main/resources/Create_test_dataset.sql
+++ src/main/resources/Create_test_dataset.sql
@@ -1,3 +1,21 @@
+/** Standardwerte für die Tabelle Rollen*/
+insert into rollen (rolle_desc) values ("Projektleiter");
+insert into rollen (rolle_desc) values ("Projektmitarbeiter");
+insert into rollen (rolle_desc) values ("Auftraggeber");
+insert into rollen (rolle_desc) values ("Product Owner");
+insert into rollen (rolle_desc) values ("Produktmanager");
+
+/** Standardwerte für die Tabelle Projektstatus*/
+insert into projektstatus (projektstatus_desc) values ("Eröffnet");
+insert into projektstatus (projektstatus_desc) values ("In Freigabe");
+insert into projektstatus (projektstatus_desc) values ("Freigegeben");
+insert into projektstatus (projektstatus_desc) values ("In Bearbeitung");
+insert into projektstatus (projektstatus_desc) values ("Abgeschlossen");
+insert into projektstatus (projektstatus_desc) values ("Abgelehnt");
+insert into projektstatus (projektstatus_desc) values ("Zurückgestellt");
+
+
+/**ZU ÜBERARBEITEN!! */
 /**Datensatz für die View: Projekt-->Details-->Status*/
 insert into projekt(idprojekt) values (111);
 insert into projektstatus (idprojektstatus, projektstatus_desc) values (0, 'Statusbeschrieb');
@@ -14,3 +32,5 @@ insert into projekt(idprojekt) values (111);
 insert into rollen (idrolle, rolle_desc) values (1, 'Scheff');
 insert into stakeholder(idstakeholder, idprojekt, idperson, idrolle) values (12, 111, 20, 1);
 insert into person (idperson, name, vorname, strasse, strassen_nr, wohnort, wohnort_plz, idfunktion, idabteilung) values (20, 'Muster', 'Max', 'Musterstrasse', 10, 'Musterhausen', 1000, 'Funktion2', 'Abteilung2');
+
+
